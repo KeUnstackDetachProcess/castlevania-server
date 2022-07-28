cf372f4efc9b64a015bbc88f1a1d756c9b93bf32
Cyclone <89141753+veil3@users.noreply.github.com>
2022-07-29 01:21:58 +0200

    minor project workflow fix

--- server/pkg/random/random.go
+++ server/pkg/random/random.go
@@ -1,28 +1,13 @@
 package random
 
 import (
-	// "bytes"
 	"crypto/rand"
-	"cv-server/pkg/log"
-	"encoding/hex"
 )
 
-// Generates a byte array which length depends on the "len" parameter
-func GenerateByteArray(len int) []byte {
-	ret := make([]byte, len) // Our return value
+func GenerateByteArray(len int) ([]byte, error) {
 
-	_, err := rand.Read(ret) // https://pkg.go.dev/crypto/rand#Read
+	ret := make([]byte, len)
+	_, err := rand.Read(ret)
 
-	if err != nil {
-		log.Error("Byte array generation failed: " + err.Error())
-		return nil
-	}
-
-	// Byte array generation was successful
-	log.Success("Byte array generation OK: " + hex.EncodeToString(ret[:]))
-
-	return ret
+	return ret, err
 }
-
-
-
